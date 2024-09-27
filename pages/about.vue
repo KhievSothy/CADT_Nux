69a119730aa0ea9e7a9e717df7ce1662b8d16d6d
Khiev Sothy <khiev.sothy@gmail.com>
2024-09-27 15:51:46 +0700

    nux project

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <Header>
+      </Header>
+      <h1>About Page</h1>
+      <p>This is the about page of our Nuxt 3 application.</p>
+      <NuxtLink to="/">Go to home Page</NuxtLink>
+    </div>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <Header></Header>
+      <h1>Home Page</h1>
+      <p>Welcome to our Nuxt 3 application!</p>
+      <NuxtLink to="/about">Go to About Page</NuxtLink>
+    </div>
+  </template>
+
